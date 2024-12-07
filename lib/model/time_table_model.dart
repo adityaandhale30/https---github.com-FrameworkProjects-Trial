59556b9b59b0e1ec4adc190c63d90ec1c15e4e32
AdiByte30 <adityaandhale30@gmail.com>
2024-12-07 20:44:40 +0530

    change in model class

--- lib/model/time_table_model.dart
+++ lib/model/time_table_model.dart
@@ -2,6 +2,7 @@ class TimetableModel {
   String subjectName;
   String date;
   String time;
+  int id = 0;
 
   TimetableModel({required this.date, required this.subjectName,required this.time});
 
